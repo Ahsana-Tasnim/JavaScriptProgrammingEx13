af3e22c78e1ba8c08bb350205bb1d7a5db741744
Ahsana-Tasnim <ahsana.tasnim@gmail.com>
2021-11-30 10:12:29 -0400

    Exercise 13

---
+++ app.js
@@ -0,0 +1,73 @@
+/*
+*   Ahsana Tasnim
+    Exercise 13
+ */
+
+// Create our first object
+let myFirstObject = {
+    myFirstStringProperty: 'My property value',
+    myFirstFloatProperty: 45.59,
+    myFunction: function(){
+        return this.myFirstFloatProperty / 2;
+    },
+    add: (a, b) => a + b
+}
+// console.log(myFirstObject);
+// console.log(myFirstObject.myFirstStringProperty);
+// console.log(myFirstObject.myFirstFloatProperty);
+// console.log(myFirstObject.myFunction());
+// console.log(myFirstObject.add(1,2));
+
+// Create Person Object
+let Person = function(firstName, lastName, dob){
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.dateOfBirth = dob,
+    this.age = function(){
+        let now = new Date();
+        let years = now.getFullYear() - this.dateOfBirth.getFullYear();
+
+        if(now.getMonth() < this.dateOfBirth.getMonth() || 
+        (now.getMonth() == this.dateOfBirth.getMonth()) &&
+        now.getDay() < this.dateOfBirth.getDay()){
+            years -= 1;
+        }
+        return years;
+    }
+    this.toString =function(){
+        return `Name: ${this.firstName} ${this.lastName} Age: ${this.age()}`
+    }
+}
+
+let person = new Person('Chris', 'Cusack', new Date(1999,7,12));
+console.log(person);
+console.log(person.toString());
+let anotherPerson = new Person('Pat', 'Thebeau', new Date(1990,2,18));
+let oneMorePerson = new Person('Stephen', 'Carter', new Date(1990,2,12));
+let finalPerson = new Person('Gary', 'Carter', new Date(1945,2,5));
+
+let people = [];
+people.push(person);
+people.push(anotherPerson);
+people.push(oneMorePerson);
+people.push(finalPerson);
+
+console.log('\nPrint all the people');
+//foeach loop call toString
+people.forEach(per => console.log(per.toString()));
+
+//Use filter method to query collection of objects based off conditions
+//Filter returns array
+console.log('\nQuery last name Carter');
+let query = people.filter(function(per) { return per.lastName == 'Carter'} );
+query.forEach(q => console.log(q.toString()));
+
+let findChris = people.filter(function(p) {return p.firstName == 'Chris' 
+&& p.lastName == 'Cusack'});
+
+if(findChris.length > 0){
+    console.log('\nRecord found:');
+    console.log(findChris[0].toString());
+}else{
+    console.log('\nRecord not found');
+}
